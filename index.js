2e71afca47b010513b28ce8fd4b75c6fa52c9b44
muhammadshah-45 <mshahaurangzeb45@gmail.com>
2024-11-27 14:07:34 +0500

    typescript start

---
+++ index.js
@@ -0,0 +1,7 @@
+function greet(name) {
+    console.log("Hello, ".concat(name, "!"));
+}
+greet(343);
+// import axios from "axios";
+// const url = 'https://jsonplaceholder.typicode.com/todos/1';
+// axios.get(url).then((response) => console.log(response.data));
