e0229c06e7a2dd48fed82350809d88d032eb1ddd
muhammadshah-45 <mshahaurangzeb45@gmail.com>
2024-11-27 14:55:34 +0500

    type topic with example

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = __importDefault(require("axios"));
 function greet(name) {
-    console.log("Hello, ".concat(name, "!"));
+    console.log(`Hello, ${name}!`);
 }
 greet(343);
-// import axios from "axios";
-// const url = 'https://jsonplaceholder.typicode.com/todos/1';
-// axios.get(url).then((response) => console.log(response.data));
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+axios_1.default.get(url).then((response) => {
+    //Response.data has properties of:
+    //id
+    //title
+    //completed
+    const todo = response.data;
+    const id = todo.id;
+    const title = todo.title;
+    const completed = todo.completed;
+    console.log(`
+     The Todo with ID: ${id}
+     Has a title of: ${title}
+     Is it finished? ${completed}
+    `);
+});
+const BioData = {
+    name: "Muhammad",
+    age: 15,
+};
+console.log(BioData);
--- index.ts
+++ index.ts
@@ -1,21 +1,50 @@
+import axios from "axios";
 function greet(name: any) {
     console.log(`Hello, ${name}!`)
 }
 greet(343);
 
-import axios from "axios";
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+
+interface Todo {
+    id:number;
+    title: string;
+    completed: boolean;
+}
 axios.get(url).then((response) => {
-   const todo = response.data;
-   const ID = todo.ID;
-   const title = todo.Title;
-   const finished = todo.finished;
+    //Response.data has properties of:
+    //id
+    //title
+    //completed
+   const todo = response.data as Todo;
+   const id = todo.id;
+   const title = todo.title;
+   const completed = todo.completed;
    console.log(`
-     The Todo with ID: ${ID}
+     The Todo with ID: ${id}
      Has a title of: ${title}
-     Is it finished? ${finished}
+     Is it finished? ${completed}
     `)
 });
 
+
+type Student = {
+  name:string;
+  age: number;
+}
+
+type StudentData ={
+    subject : string;
+    status: boolean;
+}
+
+type allData = Student & StudentData
+const BioData : allData = {
+    name: "Muhammad",
+    age: 34,
+    status:false,
+    subject:"computer science"
+}
+console.log(BioData)
